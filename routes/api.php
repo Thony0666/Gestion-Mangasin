1ee2490934656e9cdffa5358835d09c218df826b
herimaharavohaja <hei.haja.25@gmail.com>
2024-02-05 15:28:21 +0100

    feat: create api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,46 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * Route for authentication
+ */
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+});
+
+/**
+ * Public routes
+ */
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/all', [UserController::class, 'getUsers']);
+    Route::get('/{id}', [UserController::class, 'getUser'])->where(['id' => '[0-9]+']);
+    Route::post('/crup/{id?}', [UserController::class, 'storeOrUpdate'])->where('id', '[0-9]*');;
+    Route::delete('/delete/{id}', [UserController::class, 'destroy'])->where(['id' => '[0-9]+']);
+});
+
+/**
+ * Authenticated routes
+ */
+Route::group(['middleware' => 'auth:api'], function () {
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/me', [AuthController::class, 'me']);
+
+    // Admin routes
+    Route::group(['middleware' => 'role:admin'], function () {
+        Route::get('/admin', function () {
+            return ["hello" => "admin"];
+        });
+    });
+
+    // User routes
+    Route::group(['middleware' => 'role:user'], function () {
+        Route::get('/other', function () {
+            return ["hello" => "user"];
+        });
+    });
+});
+
