1297e37f141fb309af029443edd0bdd3bde1a048
herimaharavohaja <hei.haja.25@gmail.com>
2024-02-07 13:07:41 +0100

    feat: create resource supplier

--- app/Http/Resources/CreateSupplierRequest.php
+++ app/Http/Resources/CreateSupplierRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class CreateSupplierRequest extends JsonResource
+class SupplierResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -14,6 +14,15 @@ class CreateSupplierRequest extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'image' => $this->imageUrl(),
+            'email' => $this->email,
+            'address' => $this->address,
+            'city' => $this->city,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
     }
 }
