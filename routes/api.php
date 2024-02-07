889737e366347d00ccfe1c1caa8e9f857ff2a6f2
herimaharavohaja <hei.haja.25@gmail.com>
2024-02-07 13:16:58 +0100

    feat: add supplier api and article

--- routes/api.php
+++ routes/api.php
@@ -1,64 +1,47 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+use App\Http\Controllers\ArticleController;
 
 /**
- * Route for authentication
+ * Routes pour l'authentification
  */
-Route::group(['prefix' => 'auth'], function () {
+Route::prefix('auth')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
 });
 
 /**
- * Public routes
+ * Routes publiques
  */
-Route::group(['prefix' => 'user'], function () {
+Route::prefix('user')->group(function () {
     Route::get('/all', [UserController::class, 'getUsers']);
     Route::get('/{id}', [UserController::class, 'getUser'])->where(['id' => '[0-9]+']);
     Route::post('/crup/{id?}', [UserController::class, 'storeOrUpdate'])->where('id', '[0-9]*');;
     Route::delete('/delete/{id}', [UserController::class, 'destroy'])->where(['id' => '[0-9]+']);
 });
 
+Route::prefix('supplier')->group(function () {
+    Route::get('/all', [SupplierController::class, 'getSuppliers']);
+    Route::get('/{id}', [SupplierController::class, 'getSupplier'])->where(['id' => '[0-9]+']);
+    Route::post('/crup/{id?}', [SupplierController::class, 'storeOrUpdateSupplier'])->where('id', '[0-9]*');;
+    Route::delete('/delete/{id}', [SupplierController::class, 'destroy'])->where(['id' => '[0-9]+']);
+});
+
+Route::prefix('articles')->group(function () {
+    Route::get('/', [ArticleController::class, 'getArticles']);
+    Route::get('/category', [ArticleController::class, 'getArticlesWithCategory']);
+});
+
 /**
  * Authenticated routes
  */
-Route::group(['middleware' => 'auth:api'], function () {
-
+Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
     Route::get('/me', [AuthController::class, 'me']);
-
-    // Admin routes
-    Route::group(['middleware' => 'role:admin'], function () {
-        Route::get('/admin', function () {
-            return ["hello" => "admin"];
-        });
-    });
-
-    // User routes
-    Route::group(['middleware' => 'role:user'], function () {
-        Route::get('/other', function () {
-            return ["hello" => "user"];
-        });
-    });
 });
 
