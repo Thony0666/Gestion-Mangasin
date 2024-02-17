33dc30998f50d71ce4900c689748922c0168db26
Ngitangita <hei.elise.3@gmail.com>
2024-02-17 15:13:55 +0300

    add header and footer, import the file from the backoffice to app.jx

--- frontend-gestion-magasin/src/App.js
+++ frontend-gestion-magasin/src/App.js
@@ -3,6 +3,10 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Component/Login";
 import Inscription from "./Component/Inscription";
+import GestionProduction from "./Component/BackOffice/GestionProduction";
+import ComandeList from "./Component/BackOffice/ListeComande";
+import NewCat from "./Component/BackOffice/NewCat";
+import GestionPub from "./Component/BackOffice/GestionPub";
 
 const App = () => {
   return (
@@ -10,6 +14,10 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/inscription" element={<Inscription />} />
+        <Route path="/publier" element={<GestionProduction />} />
+        <Route path="/gestion/publication" element={<GestionPub />} />
+        <Route path="/list-commande" element={<ComandeList />} />
+        <Route path="/test" element={<NewCat />} />
       </Routes>
     </Router>
   );
